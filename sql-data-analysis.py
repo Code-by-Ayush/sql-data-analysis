839df023e29e161f94d268b8e18ced3fdc81312e
Ayush Joshi <83999632+Code-by-Ayush@users.noreply.github.com>
2024-08-21 18:26:50 +0530

    Update sql-data-analysis.py

--- sql-data-analysis.py
+++ sql-data-analysis.py
@@ -7,7 +7,7 @@ st.set_page_config(layout="wide")
 
 st.header("📊 SQL Data Analysis on Sample Superstore data")
 
-df = pd.read_csv("Sample - Superstore (2) (1).csv")
+df = pd.read_csv("Sample - Superstore (2) (1).csv",encoding='utf-8')
 st.dataframe(df)
 
 st.divider()
