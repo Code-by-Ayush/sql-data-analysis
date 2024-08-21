e3d264014007d9d4f7d760c5f1e76c084a6091fc
Ayush Joshi <83999632+Code-by-Ayush@users.noreply.github.com>
2024-08-21 18:30:48 +0530

    Update sql-data-analysis.py

--- sql-data-analysis.py
+++ sql-data-analysis.py
@@ -7,7 +7,7 @@ st.set_page_config(layout="wide")
 
 st.header("📊 SQL Data Analysis on Sample Superstore data")
 
-df = pd.read_excel("Sample - Superstore (2) (1).xls")
+df = pd.read_excel("Sample - Superstore (2) (1).xlsx")
 st.dataframe(df)
 
 st.divider()
